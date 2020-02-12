ab9ef9e0d238658a5334f84c672de5a76c5ffdda
vitas19 <v.bohach.2019@alumnos.urjc.es>
2020-02-12 18:19:27 +0100

    Seq0

---
+++ P0/Seq0.py
@@ -0,0 +1,10 @@
+def seq_ping():
+    print("OK!")
+
+def seq_read_fasta(filename):
+    from pathlib import Path
+    file_contents = Path(filename).read_text()
+    index_start = file_contents.find("\n")
+    seq_dna = file_contents[index_start + 1:]
+    seq_dna = seq_dna.replace("\n", "")
+    return seq_dna
