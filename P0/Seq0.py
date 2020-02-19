7a8fabb819f5eefcabc954d46381ee00d19aa9c7
vitus19 <v.bohach.2019@alumnos.urjc.es>
2020-02-19 18:23:47 +0100

    Seq0

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -7,4 +7,12 @@ def seq_read_fasta(filename):
     index_start = file_contents.find("\n")
     seq_dna = file_contents[index_start + 1:]
     seq_dna = seq_dna.replace("\n", "")
-    return seq_dna[0:20]
+    return seq_dna
+
+def seq_len(filename):
+    from pathlib import Path
+    file_contents = Path(filename).read_text()
+    index_start = file_contents.find("\n")
+    seq_dna = file_contents[index_start + 1:]
+    seq_dna = seq_dna.replace("\n", "")
+    return len(seq_dna)
