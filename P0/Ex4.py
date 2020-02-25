a65ebf7b68e5181320b4f68abfc009cbd00bce84
vitus19 <v.bohach.2019@alumnos.urjc.es>
2020-02-25 11:20:30 +0100

    Exercise 4: seq_count_base()

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -1,4 +1,8 @@
 from Seq0 import *
 filename = ["U5", "ADA", "FRAT1", "FXN"]
+bases = ["A", "C", "T", "G"]
 FOLDER = "../Session-04/"
 for gene_file in filename:
+    print("Gene", gene_file, ":")
+    for base in bases:
+        print(" ", base, ":", seq_count_base(FOLDER+gene_file, base))
