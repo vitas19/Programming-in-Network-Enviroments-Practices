fa42e6818decf47b75ed9b4d2651a4a72cf32ffd
vitus19 <v.bohach.2019@alumnos.urjc.es>
2020-02-25 13:05:41 +0100

    Seq0

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -19,21 +19,76 @@ def seq_len(filename):
 
 def seq_count_base(seq, base):
     from pathlib import Path
-    file_contents = Path(filename).read_text()
+    file_contents = Path(seq).read_text()
     index_start = file_contents.find("\n")
     seq_dna = file_contents[index_start + 1:]
     seq_dna = seq_dna.replace("\n", "")
-    count_a = 0
-    count_c = 0
-    count_t = 0
-    count_g = 0
+    count_base = 0
+    for character in seq_dna:
+        if character == base:
+            count_base += 1
+    return (count_base)
+
+def seq_count(seq):
+    from pathlib import Path
+    file_contents = Path(seq).read_text()
+    index_start = file_contents.find("\n")
+    seq_dna = file_contents[index_start + 1:]
+    seq_dna = seq_dna.replace("\n", "")
+    count_base = 0
+    bases = ["A", "T", "C", "G"]
+    d = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+    for base in bases:
+        for character in seq_dna:
+            if character == base:
+                count_base += 1
+                d[character] = count_base
+        count_base = 0
+    return (d)
+
+def seq_reverse(seq):
+    from pathlib import Path
+    file_contents = Path(seq).read_text()
+    index_start = file_contents.find("\n")
+    seq_dna = file_contents[index_start + 1:]
+    seq_dna = seq_dna.replace("\n", "")[0:20]
+    rev_dna = seq_dna[::-1]
+    return seq_dna, rev_dna
+
+def seq_complement(seq):
+    from pathlib import Path
+    file_contents = Path(seq).read_text()
+    index_start = file_contents.find("\n")
+    seq_dna = file_contents[index_start + 1:]
+    seq_dna = seq_dna.replace("\n", "")[0:20]
+    compl_seq = ""
     for character in seq_dna:
         if character == "A":
-            count_a += 1
-        elif character == "C":
-            count_c += 1
+            compl_seq = compl_seq + "T"
         elif character == "T":
-            count_t += 1
+            compl_seq = compl_seq + "A"
+        elif character == "C":
+            compl_seq = compl_seq + "G"
         elif character == "G":
-            count_g += 1
-    return (count_a, count_c, count_t, count_g)
+            compl_seq = compl_seq + "C"
+    return seq_dna, compl_seq
+
+def process_genes(seq):
+    from pathlib import Path
+    file_contents = Path(seq).read_text()
+    index_start = file_contents.find("\n")
+    seq_dna = file_contents[index_start + 1:]
+    seq_dna = seq_dna.replace("\n", "")
+    count_base = 0
+    bases = ["A", "T", "C", "G"]
+    d = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+    for base in bases:
+        for character in seq_dna:
+            if character == base:
+                count_base += 1
+                d[character] = count_base
+        count_base = 0
+    max_value = max(d.values())
+    max_keys = [k for k, v in d.items() if v == max_value]
+    return max_value, max_keys
+
