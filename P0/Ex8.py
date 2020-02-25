ff4fc3dccdf617d7532db2eb16da73be351393d2
vitus19 <v.bohach.2019@alumnos.urjc.es>
2020-02-25 13:05:14 +0100

    Exercise 8: processing the genes

---
+++ P0/Ex8.py
@@ -0,0 +1,5 @@
+from Seq0 import *
+filename = ["U5", "ADA", "FRAT1", "FXN"]
+FOLDER = "../Session-04/"
+for gene_file in filename:
+    print("Gene ",gene_file, ": Most frequent Base: ", process_genes(FOLDER+gene_file)[1])
